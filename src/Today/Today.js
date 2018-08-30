4748892dfda7d857af2b507bc668447fcdc7f912
Vaishak Kallore <vaikoovery@gmail.com>
2018-08-30 19:35:46 +0530

    Trying out testing with partial code

---
+++ src/Today/Today.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+    import './Today.css'
+    import axios from 'axios'
+    import Pusher from 'pusher-js'
+
+    class Today extends Component {
+        // Adds a class constructor that assigns the initial state values:
+        constructor () {
+            super();
+            this.state = {
+                btcprice: '',
+                ltcprice: '',
+                ethprice: ''
+            };
+        }
+        // This is called when an instance of a component is being created and inserted into the DOM.
+        componentWillMount () {
+            axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC&tsyms=USD')
+                .then(response => {
+                    // We set the latest prices in the state to the prices gotten from Cryptocurrency.
+                    if (!navigator.onLine) {
+                        this.setState({ btcprice: response.data.BTC.USD });
+                        this.setState({ ethprice: response.data.ETH.USD });
+                        this.setState({ ltcprice: response.data.LTC.USD });
+                    }
+                })
+                // Catch any error here
+                .catch(error => {
+                    console.log(error)
+                })
+                // establish a connection to Pusher
+    this.pusher = new Pusher('b7e7c44054352220ec3c', {
+        cluster: 'ap2',
+        encrypted: true
+    });
+    // Subscribe to the 'coin-prices' channel
+    this.prices = this.pusher.subscribe('coin-prices');
+        }
+        sendPricePusher (data) {
+            axios.post('/prices/new', {
+                prices: data
+            })
+              .then(response => {
+                  console.log(response)
+              })
+              .catch(error => {
+                  console.log(error)
+              })
+         }
+         componentDidMount () {
+            setInterval(() => {
+                axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC&tsyms=USD')
+                    .then(response => {
+                        this.setState({ btcprice: response.data.BTC.USD });
+                        localStorage.setItem('BTC', response.data.BTC.USD);
+
+                        this.setState({ ethprice: response.data.ETH.USD });
+                        localStorage.setItem('ETH', response.data.ETH.USD);
+
+                        this.setState({ ltcprice: response.data.LTC.USD });
+                        localStorage.setItem('LTC', response.data.LTC.USD);
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    })
+            }, 5000)
+            // We bind to the 'prices' event and use the data in it (price information) to update the state values, thus, realtime changes
+this.prices.bind('prices', price => {
+    this.setState({ btcprice: price.prices.BTC.USD });
+    this.setState({ ethprice: price.prices.ETH.USD });
+    this.setState({ ltcprice: price.prices.LTC.USD });
+  }, this);
+         }
+        // The render method contains the JSX code which will be compiled to HTML.
+        render() {
+            return (
+                <div className="today--section container">
+                    <h2>Current Price</h2>
+                    <div className="columns today--section__box">
+                        <div className="column btc--section">
+                            <h5>${this.state.btcprice}</h5>
+                            <p>1 BTC</p>
+                        </div>
+                        <div className="column eth--section">
+                            <h5>${this.state.ethprice}</h5>
+                            <p>1 ETH</p>
+                        </div>
+                        <div className="column ltc--section">
+                            <h5>${this.state.ltcprice}</h5>
+                            <p>1 LTC</p>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+    }
+
+    export default Today;
